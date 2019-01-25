53bdf754bb0062fa75017c724f3c072bd684641a
Diego Rodrigues <diego.mrodrigues11@gmail.com>
2019-01-25 09:42:00 -0200

    Sales Prediction

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name         := "Sales Prediction Project"
+version      := "1.0"
+organization := "salespred"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
+resolvers += Resolver.mavenLocal
---
+++ src/main/scala/Testing.scala
@@ -0,0 +1,16 @@
+package salespred
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+object Testing {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SparkMe Application")
+    val sc = new SparkContext(conf)
+
+    val fileName = "/hdfs/salespred/sales_train_v2.csv"
+    val lines = sc.textFile(fileName).cache
+
+    val c = lines.count
+    println(s"There are $c lines in $fileName")
+  }
+}
