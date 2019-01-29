21cf8485525b8d0504c66ca40ccc4b0788b173f2
Diego Rodrigues <diego.mrodrigues11@gmail.com>
2019-01-29 15:32:36 -0200

    Feature Selection

--- src/main/scala/tasks/MakingPredictions.scala
+++ src/main/scala/tasks/MakingPredictions.scala
@@ -22,30 +22,57 @@ import org.apache.spark.ml.evaluation.RegressionEvaluator
 
 import scala.collection.mutable
 
-class MakingPredictions()(implicit spark: SparkSession, files: FileUtils) {
+class FeatureSelection()(implicit spark: SparkSession, files: FileUtils) {
     import spark.implicits._
 
     private val vectorDataPath = "/hdfs/salespred/output/sales_vectorized.parquet"
     private val df = files.readParquet(vectorDataPath, "df")
 
+    private val disassemblerPath = "/hdfs/salespred/models/Disassembler"
+    private val selectedDataPath = "/hdfs/salespred/output/sales_selected_features.csv"
+    private val selectedVecDataPath = "/hdfs/salespred/output/sales_selected_features.parquet"
+
     def run(args: Array[String]) = {
+        val disassembler = new org.apache.spark.ml.feature.VectorDisassembler()
+            .setInputCol("features")
+
+        disassembler.write.overwrite.save(disassemblerPath)
+        
+        val disassembledDf = disassembler.transform(df)
+            .drop("features")
+
         val model = new GradientBoosting().getModel
         
+        val columns = disassembledDf.columns
+
         val featureImportances = model.featureImportances.toArray
-        val features = new mutable.ArrayBuffer[Tuple2[Int, Double]]()
+        val features = new mutable.ArrayBuffer[Tuple2[String, Double]]()
 
         for (i <- (0 until featureImportances.size)) {
-            features += new Tuple2(i, featureImportances(i))
+            features += new Tuple2(columns(i + 5), featureImportances(i))
         }
 
         val selectedFeatures = features.filter(_._2 > 0).map(_._1).toArray
+        val selectedColumns = Array("date_block_num", "date", "shop_id", "item_id") ++ selectedFeatures ++ Array("item_cnt_month")
+        val selectedColumns2 = selectedColumns.map(e => col(s"`${e}`"))
 
-        val disassembler = new org.apache.spark.ml.feature.VectorDisassembler()
-            .setInputCol("features")
+        val selectedDf = disassembledDf.select(selectedColumns2: _*)
+
+        selectedDf.write
+          .format("com.databricks.spark.csv")
+          .option("header", "true")
+          .option("mode", "overwrite")
+          .save(selectedDataPath)
         
-        val disassembledDf = disassembler.transform(df)
+        val assembler = new VectorAssembler()
+            .setInputCols(selectedFeatures)
+            .setOutputCol("features")
+        
+        val outputDf = assembler.transform(selectedDf)
 
-        disassembledDf.show(10)
-
+        output.write
+            .option("compression","none")
+            .mode("overwrite")
+            .save(selectedVecDataPath)
     }
 }
